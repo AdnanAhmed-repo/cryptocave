7121ac762880f814a6fe6feca255136b3deb237c
AdnanAhmed-repo <adnan.aahmed2002@gmail.com>
2020-09-15 14:11:58 -0700

    added CoinGrid

--- cryptocave/src/Search/index.js
+++ cryptocave/src/Search/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ConfirmButton from './ConfirmButton';
 import Page from '../Shared/Page';
+import CoinGrid from './CoinGrid';
 
 export default function(){
     return <Page name="Search">
         <ConfirmButton/>
+        <CoinGrid/>
         </Page>
 }
